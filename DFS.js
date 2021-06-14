deb7f8d5511e7696e07db546632c19ba500cc6fd
Misong Go <68773118+miniPinetree@users.noreply.github.com>
2021-06-15 00:28:12 +0900

    Update DFS.js

--- DFS.js
+++ DFS.js
@@ -22,14 +22,14 @@ for (let i = 0; i < vertices; ++i) {
 function dfs(v) {
   marked[v] = true;
 
-  for (let i of this.adj[v]) {
+  for (let w of this.adj[v]) {
     if (!marked[w]) {
       this.dfs(w);
     }
   }
 }
 
-// 모든 경로 (방문 정점) 확인 하기 (고슴도치 책보고 완성하기..)
+// 모든 경로 (방문 정점) 확인 하기 
 function Graph(v) {
   this.vertices = v;
   this.edges = 0;
@@ -39,5 +39,37 @@ function Graph(v) {
     this.adj[i] = [];
     this.adj[i].push("");
   }
-  this
+  this.addEdge = addEdge;
+  this.showGraph = showGraph;
+  this.dfs = dfs;
+  this.marked = [];
+  for (let i = 0; i < this.vertices; ++i) {
+    this.marked[i] = false;
+  }
 }
+
+function addEdge(v,w) {
+  this.adj[v].push(w);
+  this.adj[w].push(v);
+  this.edges++;
+}
+
+function showGraph() {
+  for (let i = 0; i < this.vertices; ++i) {
+    putstr(i + " -> ");
+    for (let j = 0; j < this.vertices; ++j) {
+      if (this.adg[i][j] !== undefined) putstr(this.adj[i][j] + ' ');
+    }
+  print();
+  }
+}
+
+function dfs(v) {
+  this.marked[v] = true;
+  if (this.adj[v] !== undefined) {
+    print(`Visited vertex: ${v}`);
+  }
+  this.adj[v].forEach(vertex => 
+}
+                      
+//                       (고슴도치 책보고 완성하기..)
